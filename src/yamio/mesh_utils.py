d9edd64678b5f842ff01bfc088d99e491a146360
L. F. Pereira <luisfgpereira95@gmail.com>
2021-07-06 18:32:04 +0200

    Add method do get lines from quad

--- src/yamio/mesh_utils.py
+++ src/yamio/mesh_utils.py
@@ -2,6 +2,8 @@ import numpy as np
 
 import meshio
 
+# TODO: need to verify wrong viz in paraview from converting elements
+
 
 def get_faces_from_conns(conns):
     map_to_elem = {8: from_hexa_to_quad}
@@ -14,6 +16,7 @@ def get_bnd_faces_from_conns(conns):
 
 
 def from_hexa_to_quad(conns):
+    # TODO: avoid repetitions (flag).
     bottom_face = conns[:, :4]
     top_face = conns[:, 4:]
     front_face = conns[:, [1, 2, 6, 5]]
@@ -25,13 +28,33 @@ def from_hexa_to_quad(conns):
                          back_face, right_face, left_face]
 
 
+def from_quad_to_line(conns):
+    """
+    Example:
+        [0, 1, 2, 3] -> [0, 1], [1, 2], [2, 3], [3, 0]
+    """
+    # TODO: avoid repetitions (flag).
+    line_conns = [conns[:, [i, i + 1]] for i in range(3)]
+    line_conns.append(conns[:, [-1, 0]])
+
+    return 'line', np.r_[line_conns].reshape(-1, 2)
+
+
 def from_tetra_to_tri(conns):
     # TODO: develop
     pass
 
 
+def get_bnds():
+    # TODO: more general code that works both in 2d and 3d
+    pass
+
+
 def get_bnd_faces_from_faces(faces):
     """Get boundary faces (i.e. faces that belong only to one element).
+
+    Notes:
+        Only applicable in 3d.
     """
     bnd_faces = []
     for face_cmp in faces:
@@ -50,8 +73,21 @@ def get_bnd_faces_from_faces(faces):
     return np.array(bnd_faces)
 
 
+def remove_repeated_conns():
+    """Removes repeated connectivities.
+
+    Notes:
+        Order of nodes in the connectivity does not matter.
+    """
+    pass
+
+
+def is_repeated_conn(conn):
+    pass
+
+
 def get_local_points_and_cells(points, cells):
-    """Removes unused dofs and update dofs to start at 0.
+    """Removes unused points and update dofs to start at 0.
 
     Notes:
         Used to build a mesh with a subset of initial cells.
