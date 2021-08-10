e16cdeb258aa2b453991d35837eef1a18207d5ef
L. F. Pereira <luisfgpereira95@gmail.com>
2021-08-10 16:39:35 +0200

    Add possibility of boundary representation when exporting patches

--- src/yamio/tiny_3d_engine.py
+++ src/yamio/tiny_3d_engine.py
@@ -9,15 +9,27 @@ import meshio
 
 
 from yamio.mesh_utils import get_local_points_and_cells
+from yamio.mesh_utils import get_brep
 from yamio.ensight.gold import GeoWriter
 
-# TODO: wireframe
+# TODO: go directly from mesh
 
-def write_geo_with_patches(geo_filename, mesh, **kwargs):
+
+def write_geo_with_patches(geo_filename, mesh, brep=False, **kwargs):
+    """
+    Args:
+        geo_filename (str)
+        mesh (HipMesh): Mesh object with patches.
+    """
+    # TODO: verify 2d case with brep (it is already 1d)
 
     parts = {}
     for patch_name, cell_block in mesh.bnd_patches.items():
         new_points, new_cells = get_local_points_and_cells(mesh.points, [cell_block])
+
+        if brep:
+            new_points, new_cells = get_brep(new_points, new_cells)
+
         parts[patch_name] = meshio.Mesh(new_points, new_cells)
 
     geo_writer = GeoWriter()
