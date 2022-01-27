817575bb8c3d9302f1bbcc0a3b5fcc285bc1d1ca
L. F. Pereira <luisfgpereira95@gmail.com>
2022-01-27 11:29:20 +0100

    Update handling of readers and writers register

--- src/yamio/helpers.py
+++ src/yamio/helpers.py
@@ -1,38 +1,44 @@
 
-import meshio
-
-from meshio._helpers import extension_to_filetype
-from meshio._helpers import _writer_map
-from meshio._helpers import reader_map
-
-from yamio.hip import HipReader
-from yamio.hip import HipWriter
-from yamio.ensight.gold import GeoReader
-from yamio.ensight.gold import GeoWriter
-from yamio.dolfin import write as write_dolfin
+from meshio import (
+    register_format,
+    read,
+    write,
+)
 
 
-extension_to_filetype.update({
-    '.geo': 'geo',
-    '.mesh.h5': 'hip',
-    '.mesh.xmf': 'hip'
-})
-
-reader_map.update({
-    'geo': GeoReader().read,
-    'hip': HipReader().read,
-})
-
-_writer_map.update({
-    'geo': GeoWriter().write,
-    'hip': HipWriter().write,
-    'dolfin-yamio': write_dolfin,
-})
+from yamio.ensight.gold import (
+    GeoReader,
+    GeoWriter,
+)
 
 
-def read(filename, file_format=None):
-    return meshio.read(filename, file_format=file_format)
+try:
+    import pyhip
+    has_pyhip = True
+except ImportError:
+    has_pyhip = False
+
+try:
+    import h5py
+    has_h5py = True
+except ImportError:
+    has_h5py = False
 
 
-def write(filename, mesh, file_format=None, **kwargs):
-    return meshio.write(filename, mesh, file_format=file_format, **kwargs)
+register_format('geo', ['.geo'], GeoReader().read, {'geo': GeoWriter().write})
+
+
+if has_pyhip and has_h5py:
+    from yamio.hip import (
+        HipReader,
+        HipWriter,
+    )
+
+    register_format('hip', ['.mesh.h5', '.mesh.xmf'], HipReader().read,
+                    {'hip': HipWriter().write})
+
+
+if has_h5py:
+    from yamio.dolfin import write as write_dolfin
+
+    register_format('dolfin-yamio', [], None, {'dolfin-yamio': write_dolfin})
