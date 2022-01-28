2a7606e0c5b3ce23ab5623b751bdb79211d73b93
L. F. Pereira <luisfgpereira95@gmail.com>
2022-01-28 09:31:11 +0100

    Fix read due to meshio._filetypes_from_path

--- src/yamio/helpers.py
+++ src/yamio/helpers.py
@@ -1,8 +1,11 @@
 
+from pathlib import Path
+
+import meshio
 from meshio import (
     register_format,
-    read,
     write,
+    extension_to_filetypes,
 )
 
 from yamio.hip import (
@@ -18,3 +21,14 @@ from yamio.ensight.gold import (
 register_format('geo', ['.geo'], GeoReader().read, {'geo': GeoWriter().write})
 register_format('hip', ['.mesh.h5', '.mesh.xmf'], HipReader().read,
                 {'hip': HipWriter().write})
+
+
+def read(filename, file_format=None):
+    # because meshio _filetypes_from_path does a bad job
+    if file_format is None:
+        file_formats = extension_to_filetypes.get(''.join(Path(filename).suffixes), [])
+
+        if len(file_formats) == 1:
+            file_format = file_formats[0]
+
+    return meshio.read(filename, file_format=file_format)
