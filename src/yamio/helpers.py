eb056297b3961cb20185266a627ffdf7f12930c9
L. F. Pereira <luisfgpereira95@gmail.com>
2022-01-28 08:21:54 +0100

    Fix meshio-related helpers

--- src/yamio/helpers.py
+++ src/yamio/helpers.py
@@ -1,36 +1,20 @@
 
-import meshio
+from meshio import (
+    register_format,
+    read,
+    write,
+)
 
-from meshio._helpers import extension_to_filetype
-from meshio._helpers import _writer_map
-from meshio._helpers import reader_map
-
-from yamio.hip import HipReader
-from yamio.hip import HipWriter
-from yamio.ensight.gold import GeoReader
-from yamio.ensight.gold import GeoWriter
-
-
-extension_to_filetype.update({
-    '.geo': 'geo',
-    '.mesh.h5': 'hip',
-    '.mesh.xmf': 'hip'
-})
-
-reader_map.update({
-    'geo': GeoReader().read,
-    'hip': HipReader().read,
-})
-
-_writer_map.update({
-    'geo': GeoWriter().write,
-    'hip': HipWriter().write,
-})
+from yamio.hip import (
+    HipReader,
+    HipWriter,
+)
+from yamio.ensight.gold import (
+    GeoReader,
+    GeoWriter,
+)
 
 
-def read(filename, file_format=None):
-    return meshio.read(filename, file_format=file_format)
-
-
-def write(filename, mesh, file_format=None, **kwargs):
-    return meshio.write(filename, mesh, file_format=file_format, **kwargs)
+register_format('geo', ['.geo'], GeoReader().read, {'geo': GeoWriter().write})
+register_format('hip', ['.mesh.h5', '.mesh.xmf'], HipReader().read,
+                {'hip': HipWriter().write})
